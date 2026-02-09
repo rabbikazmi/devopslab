8b40147d453f96adc0816e4cc8a5fb0d49d2a5b8
rabbikazmi <azmirabbika@gmail.com>
2026-02-09 15:38:38 +0530

    Initial commit using Python

---
+++ exp11.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+def run(cmd):
+    subprocess.run(cmd, shell=True)
+
+# initialize Git repository
+if not os.path.exists(".git"):
+    run("git init")
+    print("Git repository initialized")
+
+with open("sample.txt", "w") as f:
+    f.write("Initial commit file")
+
+run("git add .")
+run('git commit -m "Initial commit using Python"')
+
+print("Commit created successfully")
